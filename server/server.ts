9a88f3e96a6a13b2f3dea9142052b0d62ae7d102
navaskm <navaskm228@gmail.com>
2026-09-18 14:53:45 +0530

    created API for restaurants

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,7 @@ import express, { NextFunction, Request, Response } from 'express';
 import cors from "cors";
 import connectDB from "./config/db.js";
 import authRouter from "./routes/authRoutes.js";
+import restaurantRoute from "./routes/restaurantRoutes.js";
 
 const app = express();
 await connectDB();
@@ -17,6 +18,7 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Server is Live!');
 });
 app.use('/api/auth', authRouter);
+app.use('/api/restaurants', restaurantRoute);
 
 // GLOBAL ERROR HANDLER
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
